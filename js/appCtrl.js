d7a285c2787ce43db20c99582adeda7fd592738e
shivanyshenoy <shivany.shenoy@panerabread.com>
2014-06-08 12:32:45 -0500

    Added angular, carousel.

---
+++ js/appCtrl.js
@@ -0,0 +1,7 @@
+function AppCtrl($scope) {
+    $scope.dashboardMsg = "RC456Mason is an app that runs on-site analysis of basic RCC structure and" +
+        " its elements using limit state of collapse method (IS 456-2000). " +
+        "Further, users can optimize design to conserve both cost and " +
+        "energy intensive materials such as cement and steel beside its compliance as per IS code for " +
+        "structure integrity and safety. For more details click on app coverage.";
+}
